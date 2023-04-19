96e2f996762f3628ed14e8dd33b9d84fa8b7722a
s9551 <s95510691@gmail.com>
2023-04-19 22:08:13 +0800

    fake page v5

--- FakePage/main.js
+++ FakePage/main.js
@@ -1,9 +1,8 @@
 let togglePassword = document.getElementById("togglePassword");
 let password = document.getElementById("input_password");
 
-togglePassword.addEventListener("click", PasswordChangeType);
-
-function PasswordChangeType(){
+togglePassword.addEventListener("click", function (){
     let type = password.getAttribute('type') === 'password' ? 'text' : 'password';
     password.setAttribute("type", type);
-}
+    this.classList.toggle('fa-eye-slash');
+});
