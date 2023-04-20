34d2b6fa0098c8c951b986d9e0435edb3bcf6d8b
s9551 <s95510691@gmail.com>
2023-04-20 22:11:44 +0800

    fake page v9

--- FakePage/main.js
+++ FakePage/main.js
@@ -1,9 +1,9 @@
 let togglePassword = document.getElementById("togglePassword");
 let password = document.getElementById("input_password");
-let isMoblie = false;
+let isMobile = false;
 
 if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)) {
-    isMoblie = true;
+    isMobile = true;
     let loginBlock = document.getElementById('loginBox');
     IncreaseSize(loginBlock);
 }
@@ -14,25 +14,24 @@ togglePassword.addEventListener('click', function() {
     this.classList.toggle('fa-eye-slash');
 });
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', Resize);
+
+function Resize(){
     let viewport_width = window.innerWidth;
     let loginBlock = document.getElementById('loginBox');
-    if (!isMoblie && viewport_width <= 540)
-        IncreaseSize(loginBlock);
+    if (isMobile || viewport_width <= 540)
+        MobileStyle(loginBlock);
     else
-        DecreaseSize(loginBlock);
-});
-
-function IncreaseSize(loginBlock){
-    loginBlock.style.width = '100%';
-    loginBlock.style.height = 'auto';
-    loginBlock.style.margin = '0 auto 0 auto';
-    loginBlock.style.padding = '100px 24px 40px 0px';
+        DesktopStyle(loginBlock);
 }
 
-function  DecreaseSize(loginBlock){
-    loginBlock.style.width = '312px';
-    loginBlock.style.height = '100%';
-    loginBlock.style.marginTop = '8rem auto 300 auto';
-    loginBlock.style.padding = '36px 24px 36px 24px';
+function MobileStyle(loginBlock){
+    document.querySelector("link[href='styleComputer.css']").href = "styleMobile.css";
+
 }
+
+function DesktopStyle(loginBlock){
+    document.querySelector("link[href='styleMobile.css']").href = "styleComputer.css";
+}
+
+Resize();
